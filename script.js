420f1f86f3e51f4ddcf1f8e898e45fb469f92ecb
iskandar <106257364+Iskandar-web-dev@users.noreply.github.com>
2022-10-30 17:32:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // //1
-// let int = prompt('any nunmber')
-// if (int % 2 ===0) {
-//     alert('Чётное')
-// } else {
-//     alert('не чётное')
-// }
+let int = prompt('any nunmber')
+if (int % 2 ===0) {
+    alert('Чётное')
+} else {
+    alert('не чётное')
+}
+
+//2
+// let user = prompt('Как вас зовут?').toLowerCase().trim()
+// if(user[0] === 'b' && user.length % 2  === 0 ) {
+//     alert('Ваше имя ' + user + ' Количество букв: Чётноe')
+// }   else{
+//     alert('Ваше имя не с буквы "b"' + ' Количество букв: Нечётное')
+// }  
+
