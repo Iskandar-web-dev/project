658c8b4ca419df3e4cdcdcd6f5f9d08d626cb553
iskandar <106257364+Iskandar-web-dev@users.noreply.github.com>
2022-12-05 15:02:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,96 @@
-let name = prompt('Имя')
-let surName = prompt('Фамилия')
-let net = prompt('Какое окончания хотите для емайла?')
-let rezultat = name + surName + '@' + net
-console.log(rezultat);
+// ИСХОДНЫЕ ДАННЫЕ НЕ ТРОГАТЬ!*
 
-// let rezultat = '';
-// let word = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-// let max_position = word.length - 1;
-//     for( i = 0; i < 10; ++i ) {
-//         position = Math.floor ( Math.random() * max_position );
-//         rezultat = rezultat + word.substring(position, position + 1);
-//     }
+let discount = prompt('Какая скидка?')
+
+let totalSale = Number
+
+let total = Number
+
+let max = Object
+
+let min = Object
+
+let average = Number
+
+let arr = [
+
+    {
+
+        name: 'Milk',
+
+        price: 3.25
+
+    },
+
+    {
+
+        name: 'Coffee',
+
+        price: 1.5
+
+    },
+
+    {
+
+        name: 'Ice Cream',
+
+        price: 7.85
+
+    },
+
+    {
+
+        name: 'Tomatos',
+
+        price: 4.14
+
+    },
+
+    {
+
+        name: 'Onion',
+
+        price: 2.25
+
+    }
+]
+
+let arr_sale = []
+
+// 1. Сохранить самый дорогой товар в переменную`max`*
+// 2. Сохранить самый дешевый товар в переменную`min`*
+// 3. Сохранить общую цену без скидок в переменную`total`*
+// 4. Сохранить общую цену со скидкой в переменную`totalSale`*
+// 5. Сохранить элементы из массива arr с обновленной ценой (цена со скидкой) в переменную `arr_sale`*
+// 6. Сохранить в переменной`average` среднюю цену всех продуктов без скидок*
+// * Писать весь код в функции `setup()`*
+// ТРИ ОЦЕНКИ. ЧИСТОТА КОДА, ЛОГИКА РАБОТЫ, УНИКАЛЬНОСТЬ КОДА*
+
+const setup = () => {
+    //1
+    max = arr.reduce((a, b) => a.price > b.price ? a : b)
+    //2
+    min = arr.reduce((a, b) => a.price < b.price ? a : b)
+    //3
+    total = 0
+    arr.forEach((item) => {
+        total += item.price
+    })
+    //4
+    totalSale = total - (discount * total / 100)
+    //5
+   
+    for(let item of arr) {
+            arr_sale.push(item);
+        }
+        console.log(arr_sale);
+
+{    
     
-// let num = Math.random() + rezultat
-// console.log(num);
+   }
+   //6
+   average = total/5
+   console.log(average);
+}
+
+setup()
